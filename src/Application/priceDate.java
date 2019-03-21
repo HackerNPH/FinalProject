3e6ca957d3a9ab7853946c64051ac4b82ca117b9
Nick Hattrup <32020826+HackerNPH@users.noreply.github.com>
2019-03-20 22:41:56 -0400

    Add files via upload
    The new files!

---
+++ src/Application/priceDate.java
@@ -0,0 +1,28 @@
+package Application;
+public class priceDate {
+	static String priceDate;
+	static String opening_Price;
+	static String closing_Price;
+	
+
+	priceDate() {
+	}
+	
+	public priceDate(String d, String o, String c) {
+		priceDate = d;
+		opening_Price = o;
+		closing_Price = c;
+	}
+	
+	public static String getDate() {
+		return priceDate;
+	}
+	public static String getOpeningPrice() {
+		return opening_Price;
+	}
+	public static String getClosingPrice() {
+		return closing_Price;
+	}
+	
+
+}
